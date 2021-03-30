dca47211902815b805d8dbee3fe4578f0c73a88a
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-03-31 00:38:28 +1300

    Added delete option

---
+++ networthapp/AccountDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountDetailView.swift
+//  networthapp
+//
+//  Created by Paulo Orquillo on 31/03/21.
+//
+
+import SwiftUI
+
+struct AccountDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountDetailView()
+    }
+}
